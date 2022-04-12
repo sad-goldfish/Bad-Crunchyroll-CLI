09a3877f1783da27e76aba5dc30c9a62f76a3aa2
sad-goldfish <98661640+sad-goldfish@users.noreply.github.com>
2022-04-12 20:31:15 +0100

    Create links.sh

---
+++ src/links.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+grep -hE "<loc>[^<>]*<\/loc>" *.html -o |\
+sed "s/<loc>\([^<>]*\)<\/loc>/\1/g" |\
+grep -vE "/comments$|/videos$|/reviews$|/discussions$|/more$|forumcategory|/en-gb/|/ru/|/fr/|/de/|/ar/|/it/|/pt-pt/|/pt-br/|/es-es/" |\
+grep "//[^/]*/[^/]*/[^/]*$"

f90685f1cdf78cc6108b23b46e0fc374b731cfed
sad-goldfish <98661640+sad-goldfish@users.noreply.github.com>
2022-04-12 21:18:55 +0100

    Update sitemap.py

--- src/sitemap.py
+++ src/sitemap.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import re
+import sys
 
 googleURL = "https://www.crunchyroll.com/sitemap"
 
@@ -11,8 +12,11 @@ f=open("sitemap.txt","w")
 f.write(content)
 f.close()
 maps = re.findall("<loc>([^<>]*)<\/loc>",content)
-for i in range(len(maps)):
+for i in range(int(sys.argv[1]) if len(sys.argv) > 1 else 0,len(maps)):
     f=open(f"{i}.html","w")
     browser.get(maps[i])
     f.write(browser.page_source)
     f.close()
+f=open("latest.txt","w")
+f.write(str(len(maps)-1))
+f.close

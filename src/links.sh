b77d43670d3021c3f2e133f983abce1222f6e20f
sad-goldfish <98661640+sad-goldfish@users.noreply.github.com>
2022-04-12 20:40:39 +0100

    Update links.sh

--- src/links.sh
+++ src/links.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 grep -hE "<loc>[^<>]*<\/loc>" *.html -o |\
-sed "s/<loc>\([^<>]*\)<\/loc>/\1/g" |\
+sed "s/<loc>https:\/\/www\.crunchyroll\.com\([^<>]*\)<\/loc>/\1/g" |\
 grep -vE "/comments$|/videos$|/reviews$|/discussions$|/more$|forumcategory|/en-gb/|/ru/|/fr/|/de/|/ar/|/it/|/pt-pt/|/pt-br/|/es-es/|/es/" |\
-grep "//[^/]*/[^/]*/[^/]*$"
+grep "^/[^/]*/[^/]*$"

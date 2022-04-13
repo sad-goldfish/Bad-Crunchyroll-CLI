cce3265726525443afbdb7700b2ff79421e2629c
sad-goldfish <98661640+sad-goldfish@users.noreply.github.com>
2022-04-13 15:55:00 +0100

    Update populate.sh

--- src/populate.sh
+++ src/populate.sh
@@ -5,9 +5,15 @@ SDIR="$(readlink -f "$(dirname "$0")")"
 cd ~/.local/bad-crunchyroll-cli/
 if [ -e ~/.local/bad-crunchyroll-cli/latest.txt ]
 then
+  LATEST="$(cat ~/.local/bad-crunchyroll-cli/latest.txt)"
   python3 "${SDIR}"/sitemap.py "$(cat ~/.local/bad-crunchyroll-cli/latest.txt)"
 else
   python3 "${SDIR}"/sitemap.py
 fi
 find -name "*.html" -exec xz -f --verbose {} +
-sh "${SDIR}"/links.sh > ~/.local/bad-crunchyroll-cli/links.txt
+if [ -e ~/.local/bad-crunchyroll-cli/links.txt -a ! -z "$LATEST" ]
+then
+  sh "${SDIR}"/links.sh "$LATEST" | { grep -vFf ~/.local/bad-crunchyroll-cli/links.txt || true ; } >> ~/.local/bad-crunchyroll-cli/links.txt
+else
+  sh "${SDIR}"/links.sh > ~/.local/bad-crunchyroll-cli/links.txt
+fi

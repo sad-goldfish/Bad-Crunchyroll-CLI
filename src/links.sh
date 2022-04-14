516a57a8f23542cc0a997ae587e9820d5cca7e28
sad-goldfish <98661640+sad-goldfish@users.noreply.github.com>
2022-04-14 20:21:02 +0100

    Update links.sh

--- src/links.sh
+++ src/links.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-{
-	if [ ! -z "$1" ]
-	then
-		find -name "*.html.xz" | sed -E 's/\.\/([0-9]*).html.xz/\1/g' | awk "{if (\$1 >= $1) print \$1\".html.xz\"}" | xargs xzcat
-	else
-		find -name "*.html.xz" -exec xzcat {} +
-	fi
-} |\
+cat /dev/stdin |\
 grep -hE "<loc>[^<>]*<\/loc>" -o |\
 sed "s/<loc>https:\/\/www\.crunchyroll\.com\([^<>]*\)<\/loc>/\1/g" |\
 grep -vE "/comments$|/videos$|/reviews$|/discussions$|/more$|forumcategory|/en-gb/|/ru/|/fr/|/de/|/ar/|/it/|/pt-pt/|/pt-br/|/es-es/|/es/" |\

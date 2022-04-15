c61b4c81d913665e7ee5160163194f25fdb6b807
sad-goldfish <98661640+sad-goldfish@users.noreply.github.com>
2022-04-15 16:12:17 +0100

    Update stream.sh

--- src/stream.sh
+++ src/stream.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat ~/.local/bad-crunchyroll-cli/links.txt | dmenu -i -l 12 | xargs -I 1# mpv https://www.crunchyroll.com/1#
+cat ~/.local/bad-crunchyroll-cli/links.txt | dmenu -i -l 12 | xargs -I 1# mpv https://www.crunchyroll.com/1# --ytdl --script-opts=ytdl_hook-try_ytdl_first=yes --slang=enUS
